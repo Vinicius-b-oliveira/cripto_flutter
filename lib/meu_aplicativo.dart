a6bb7192b5913fd8dae487cb8a8e8089080008ea
Vinicius <viniciusbuenodeoliveira2017@gmail.com>
2025-03-28 11:14:23 -0300

    Iniciando curso

---
+++ lib/meu_aplicativo.dart
@@ -0,0 +1,19 @@
+import 'package:cripto_flutter/pages/moedas_page.dart';
+import 'package:flutter/material.dart';
+
+class MeuAplicativo extends StatelessWidget {
+  const MeuAplicativo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moedasbase',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MoedasPage(),
+    );
+  }
+}
