74db3c2d3c737b7df50fe13a20cd6905beee3956
Vinicius <viniciusbuenodeoliveira2017@gmail.com>
2025-04-03 11:12:59 -0300

    Add hive

--- lib/config/app_settings.dart
+++ lib/config/app_settings.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class AppSettings extends ChangeNotifier {
-  late SharedPreferences _prefs;
+  // late SharedPreferences _prefs;
+  late Box box;
   Map<String, String> locale = {'locale': 'pt_BR', 'name': 'R\$'};
 
   AppSettings() {
@@ -15,19 +16,24 @@ class AppSettings extends ChangeNotifier {
   }
 
   Future<void> _startPreferences() async {
-    _prefs = await SharedPreferences.getInstance();
+    // _prefs = await SharedPreferences.getInstance();
+    box = await Hive.openBox('preferencias');
   }
 
   _readLocale() {
-    final local = _prefs.getString('local') ?? 'pt_BR';
-    final name = _prefs.getString('name') ?? 'R\$';
+    // final local = _prefs.getString('local') ?? 'pt_BR';
+    // final name = _prefs.getString('name') ?? 'R\$';
+    final local = box.get('local') ?? 'pt_BR';
+    final name = box.get('name') ?? 'R\$';
     locale = {'locale': local, 'name': name};
     notifyListeners();
   }
 
   setLocale(String local, String name) async {
-    await _prefs.setString('local', local);
-    await _prefs.setString('name', name);
+    // await _prefs.setString('local', local);
+    // await _prefs.setString('name', name);
+    await box.put('local', local);
+    await box.put('name', name);
     await _readLocale();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:cripto_flutter/config/app_settings.dart';
+import 'package:cripto_flutter/config/hive_config.dart';
 import 'package:cripto_flutter/repositories/favoritas_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
   runApp(
     MultiProvider(
       providers: [
